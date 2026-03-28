486cf7f376f3fa6e12b06bcd2f7ee6e68ea30c19
Jman87 <juanjomartinezv2707@gmail.com>
2026-03-27 23:19:22 -0400

    Added abilities class and prototype dash

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public abstract class Ability : MonoBehaviour
+{
+    // ability ideas
+    /*
+        1. Dash
+        2. Super Shield / AOE attack
+        3. Long strike
+    */
+
+    public float skillCooldown = 5f; // the cooldown of the skill
+    public float lastUsed; // when the skill was last used.
+
+    public bool canUse () // bool to check whether the skill can be used.
+    {
+        return Time.time >= skillCooldown - lastUsed;
+    }
+
+    public void startCooldown () // marks when skill enters cooldown
+    {
+        lastUsed = Time.time;
+    }
+    
+    public abstract void Activate(); // nothing in the base class, inherited objects will have the actual details.
+    public abstract void Upgrade(); // similar to Activate
+    
+
+}
---
+++ Assets/Scripts/Skill Manager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SkillManager : MonoBehaviour
+{
+    public List<Ability> abilities; // makes a list for the abilities
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) //PLACEHOLDER KEYS, MAKE JORDON CHANGE THEM.
+        {
+            abilities[0].Activate();
+            // activates dash
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            abilities[1].Activate();
+            // activates AOE
+        }
+    }
+}
